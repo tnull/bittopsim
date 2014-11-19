dfdee7d00fa42c8245d7b31f708c4be3d4c657b1
Elias Rohrer <rohrer@informatik.hu-berlin.de>
2014-11-19 15:05:51 +0100

    added comments

--- src/bctopologysim.cpp
+++ src/bctopologysim.cpp
@@ -1,13 +1,33 @@
+/**
+ * @mainpage bctopologysim
+ *
+ * Bitcoin topology simulator
+ * @author Elias Rohrer
+ */
+
+/**
+ * @file bctopologysim.cpp
+ *
+ * @brief This is where everything starts, the application is initialzied and run.
+ * 
+ */
+
 #include <iostream>
 
 using namespace std;
 
+/**
+ * @brief AppInit is where the application starts
+ */
 bool AppInit( int argc, char* argv[] )
 {
 	cout << "Starting bctopologysim" << endl;
 	return true;
 }
 
+/**
+ * @brief main is just there to call AppInit
+ */
 int main(int argc, char** argv) 
 {
 	if (!AppInit(argc, argv)) return -1;
