ff6c68ffbd300f108db92f5280c0832c3513aa83
Elias Rohrer <rohrer@informatik.hu-berlin.de>
2015-02-17 11:00:28 +0100

    Improved test script

--- dot_to_png.sh
+++ dot_to_png.sh
@@ -1,13 +1,20 @@
-#DOTCMDS=(dot neato twopi circo fdp sfdp osage)
-DOTCMDS=(neato twopi fdp sfdp)
-NODENUM=(10 20 30 40 50 100 150 200 250 500 1000)
-for i in ${NODENUM[*]}; do
-	echo "Running for $i nodes:"
-	mkdir -p ./graphs
-	./src/btctopologysim 1 $i 86400 ./graphs/graph.gv > /dev/null # test with 50 server nodes
-	#./btctopologysim  $i > /dev/null
+SIMCMD=./src/btctopologysim
+DOTCMDS=(dot neato twopi circo fdp sfdp osage)
+#DOTCMDS=(neato twopi fdp sfdp)
+GRAPHFOLDER=./graphs
+GRAPHFILE=$GRAPHFOLDER/graph.gv
+SIMTIME=86400
+SIMSRVNODES=50
+SIMCLTNODES=(10 20 30 40 50 100 150 200 250 500 1000)
+
+#cleanup before start:
+rm $GRAPHFOLDER/*
+for i in ${SIMCLTNODES[*]}; do
+	echo "Running for $SIMSRVNODES server nodes and $i client nodes:"
+	mkdir -p $GRAPHFOLDER
+	$SIMCMD $SIMSRVNODES $i $SIMTIME $GRAPHFILE > /dev/null
 	for j in ${DOTCMDS[*]}; do
 		echo "\tRunning $j:"
-		$j -Tpng -Gsize=9,15\! -Gdpi=100 ./graphs/graph.gv -o ./graphs/$i-$j.png
+		$j -Tpng -Gsize=9,15\! -Gdpi=100 $GRAPHFILE -o $GRAPHFOLDER/$i-$j.png
 	done
 done
