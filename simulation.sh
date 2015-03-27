22504b78db5a06f07235f98c5d581d183495bc90
Elias Rohrer <rohrer@informatik.hu-berlin.de>
2015-03-27 13:40:44 +0100

    Simulation now is parallel, hoping it will get ready.

--- simulation.sh
+++ simulation.sh
@@ -4,11 +4,14 @@ SIMTIME=864000
 SIMSRVNODES=(50 100 500 1000 3000 6500 10000)
 SIMCLTNODES=(0 50 100 500 1000 3000 5000 10000 25000)
 
+rm .argfile
 mkdir -p log
+echo "Generating Args file"
 for i in ${SIMSRVNODES[*]}; do
 	for j in ${SIMCLTNODES[*]}; do
-		echo "Running $SIMCMD for $i server nodes and $j client nodes, in $SIMTIME:"
-		echo $SIMCMD $i $j $SIMTIME > log/bittopsim-$i-$j-$SIMTIME.log
-		$SIMCMD $i $j $SIMTIME >> log/bittopsim-$i-$j-$SIMTIME.log
+		echo "\"$i $j $SIMTIME >> log/bittopsim-$i-$j-$SIMTIME.log\"" >> .argfile
 	done
 done
+
+#cat argfile|xargs -I % -P4 -t sh -c "$SIMCMD %"
+#rm .argfile
